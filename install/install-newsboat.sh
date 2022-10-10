f2d8fd551b3a3567ece321901cd28d8adb290b4e
bimmerr2019 <bimmer@Interamnia>
2022-10-10 11:41:13 -0700

    fix newsboat

--- install/install-newsboat.sh
+++ install/install-newsboat.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
-
-ln -s $HOME/Nextcloud/.newsboat $HOME
+ln -sf "$DOTFILES/newsboat/urls" "$HOME/.newsboat"
+ln -sf "$DOTFILES/newsboat/config" "$HOME/.newsboat"
+ln -s $HOME/Nextcloud/.gnupg $HOME
+ln -s $HOME/Nextcloud/.ssh $HOME
